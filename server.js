90d652174dbfb1eb8f791663a51a5bece74c8cd3
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-08-16 13:52:47 -0400

    chat room template

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log("running")
+
+
+app.get("/", function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+io.sockets.on("connection", function(socket){
+	connections.push(socket);
+	console.log("Connected: %s sockets connected", connections.length);
+	socket.on("disconnect", function(data){
+		connections.splice(connections.indexOf(socket), 1)
+		console.log("Disconnected: %s sockets connected", connections.length);
+	});
+}); 
