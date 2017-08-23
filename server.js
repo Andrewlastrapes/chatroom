f78949b95a0044a9ec464fe5419c213de5a32bc5
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-08-22 22:17:54 -0400

    added form page

--- server.js
+++ server.js
@@ -5,8 +5,68 @@ const io = require("socket.io")(server);
 const _ = require('lodash');
 const path = require("path");
 
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var db = require('mongoose');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+app.set('views', path.join(__dirname , 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+app.use(session({
+	secret: 'secret',
+	saveUnitialized: true,
+	resave: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 
+app.use(flash());
+
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.flash("error");
+  next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -81,5 +141,5 @@ io.on("connection", function(socket){
 
 
 server.listen(4000, () => { 
-	console.log("running")
+	console.log("listening")
 });
